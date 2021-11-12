2a8b60024cc0fedf867bb8d0eaccd99735f3b00d
Arizbet Pena <beth@Arizbets-MacBook-Pro.local>
2021-11-12 13:10:00 -0800

    Complete Step 3.3

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ItemList.as_view(), name="item_list"),
+    path('item/add/', views.ItemCreate.as_view(), name="item_form"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Item
+from main_app import models
 # Create your views here.
 
 class ItemList(ListView):
     model = Item
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
